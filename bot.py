375cc473fb724fa375cc103851ebd4e5c9ef7700
addsons <addsons@gmail.com>
2025-08-10 11:43:58 -0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,11 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Olá! Eu estou online 24h 😎")
+
+token = os.getenv("BOT_TOKEN")
+app = ApplicationBuilder().token(token).build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
